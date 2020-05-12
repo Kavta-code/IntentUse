1dbf9bf6352685a58bfbefd561ecd69e3db23362
Kavta-code <kavitha.y05@gmail.com>
2020-05-12 11:45:06 +0530

    Change Activity Using Intent

---
+++ app/src/main/java/com/example/intentactivitychange/FirstActivity.java
@@ -0,0 +1,26 @@
+package com.example.intentactivitychange;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
